e7ab732db01d50605e286f1d7b7eea8f0bff074f
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-07-16 16:07:17 +0530

    Add files via upload

---
+++ new.ts
@@ -0,0 +1,15 @@
+var num2 : number = 20 ;
+console.log(num2);  //20
+
+var num3 : string = "najim";
+console.log(num3); //najim
+
+var num5 = true;
+console.log(num5); //true
+
+function addition(a : number , b : number) : number {
+    return a + b ;
+}
+
+console.log(addition(10,20));
+
